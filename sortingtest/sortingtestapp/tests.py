04b5710ec4abcb2f68759b3985f04f4519edaf74
krvc <krvc.it@gmail.com>
2013-09-26 16:46:46 +0530

    Pep8 compliant

--- sortingtest/sortingtestapp/tests.py
+++ sortingtest/sortingtestapp/tests.py
@@ -1,30 +1,31 @@
 from django.test import TestCase
-
 from django.shortcuts import render_to_response
 from django.template import Template, Context
 from django.http import HttpRequest
-
 from django.test import Client
 from django.core.handlers.wsgi import WSGIRequest
 from django.test.client import Client
 
+
 class RequestFactory(Client):
+
     """
     Class that lets you create mock Request objects for use in testing.
-    
+
     Usage:
-    
+
     rf = RequestFactory()
     get_request = rf.get('/hello/')
     post_request = rf.post('/submit/', {'foo': 'bar'})
-    
+
     This class re-uses the django.test.client.Client interface, docs here:
     http://www.djangoproject.com/documentation/testing/#the-test-client
-    
-    Once you have a request object you can pass it to any view function, 
+
+    Once you have a request object you can pass it to any view function,
     just as if that view had been hooked up using a URLconf.
-    
+
     """
+
     def request(self, **request):
         """
         Similar to parent class, but returns the request object as soon as it
@@ -46,6 +47,7 @@ class RequestFactory(Client):
 
 
 class TestTemplatetags(TestCase):
+
     def test_sort_links(self):
         "Test that using sort_link does not cause any error."
         template_str = """
@@ -54,7 +56,7 @@ class TestTemplatetags(TestCase):
         """
         template = Template(template_str)
         rendered = template.render(Context({}))
-        
+
     def test_sort_links_results(self):
         template_str = """
         {% load sorting %}
@@ -63,7 +65,7 @@ class TestTemplatetags(TestCase):
         template = Template(template_str)
         rendered = template.render(Context({}))
         print rendered
-        
+
         template_str = """
         {% load sorting %}
         {% sort_link "link text" "field_name" "Visible name" %}
@@ -71,9 +73,9 @@ class TestTemplatetags(TestCase):
         template = Template(template_str)
         rendered = template.render(Context({}))
         print rendered
-        
+
     def test_auto_sort(self):
-        from sortingtestapp.models import SortableThingy
+        from .sortingtestapp.models import SortableThingy
         sortable = SortableThingy.objects.all()
         template_str = """
         {% load sorting %}
@@ -81,9 +83,9 @@ class TestTemplatetags(TestCase):
         """
         template = Template(template_str)
         rendered = template.render(Context({"sortable": sortable}))
-        
+
     def test_auto_sort_values(self):
-        from sortingtestapp.models import SortableThingy
+        from .sortingtestapp.models import SortableThingy
         sortable = SortableThingy.objects.all()
         template_str = """
         {% load sorting %}
@@ -99,6 +101,3 @@ class TestTemplatetags(TestCase):
         c.get("/sorttest/?sort_by=roll")
         c.get("/sorttest/xxx/?sort_by=name")
         c.get("/sorttest/xxx/?sort_by=parent")
-
-        
-        
