3f4c13da01a4fb22559400676078ff43513fdae6
krvc <krvc.it@gmail.com>
2013-09-26 16:45:42 +0530

    Pep8 compliant

--- sortingtest/sortingtestapp/models.py
+++ sortingtest/sortingtestapp/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 
+
 class SortableThingy(models.Model):
-    name = models.CharField(max_length = 100)
-    roll =  models.IntegerField(default = 10)
-    
+    name = models.CharField(max_length=100)
+    roll = models.IntegerField(default=10)
+
     def __unicode__(self):
         return "%s, %s" % (self.name, self.roll)
-    
-    
+
+
 class SortableThingy2(models.Model):
     parent = models.ForeignKey(SortableThingy)
-    name = models.CharField(max_length = 100)
-    
-    
+    name = models.CharField(max_length=100)
+
     def __unicode__(self):
         return self.name
