99251b68ad5303b569fe2b4e3f5de7abc5d80b8e
krvc <krvc.it@gmail.com>
2013-09-26 16:50:09 +0530

    Removed deprications

--- sortingtest/urls.py
+++ sortingtest/urls.py
@@ -1,10 +1,9 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
 
 
 urlpatterns = patterns('',
-    url(r'^sorttest/', include('sortingtestapp.urls')),
-    url(r'^admin/(.*)', admin.site.root),
-)
+                       url(r'^sorttest/', include('sortingtestapp.urls')),
+                       url(r'^admin/', include(admin.site.urls)), )
