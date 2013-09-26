ba7be17e74d0c8a498deae75e355b294d71da8b0
krvc <krvc.it@gmail.com>
2013-09-26 16:47:51 +0530

    Pep8 compliant

--- sortingtest/sortingtestapp/views.py
+++ sortingtest/sortingtestapp/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from sortingtestapp.models import SortableThingy, SortableThingy2
 
-def index(request):    
+
+def index(request):
     s1 = SortableThingy.objects.all()
     return render_to_response("sortingtestapp/index.html", locals(), RequestContext(request))
 
 
-def index2(request):    
+def index2(request):
     s2 = SortableThingy2.objects.all()
     return render_to_response("sortingtestapp/index2.html", locals(), RequestContext(request))
