5dfcdccd4a7d28fa73194cab4754c5d30b74b538
krvc <krvc.it@gmail.com>
2013-09-26 16:49:11 +0530

    Pep8 compliant

--- sortingtest/sortingtestapp/urls.py
+++ sortingtest/sortingtestapp/urls.py
@@ -1,10 +1,5 @@
-from django.conf.urls.defaults import *
-
-
-
+from django.conf.urls import patterns
 
 urlpatterns = patterns('sortingtestapp.views',
-    (r'^$', 'index'),
-    (r'^xxx/$', 'index2'),
-    
-)
+                      (r'^$', 'index'),
+                      (r'^xxx/$', 'index2'), )
